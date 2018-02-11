3215c6aa9601527f2d4fe6e840e622c4664769ef
Haichao Wang <wanghaichao2195@gmail.com>
2018-02-11 21:39:34 +0000

    add google map feature

--- app.js
+++ app.js
@@ -16,9 +16,11 @@ var express     = require("express"),
 var     commentRoutes    = require("./routes/comments"),
         campgroundRoutes = require("./routes/campgrounds"),
         indexRoutes       = require("./routes/index")
-    
-mongoose.connect(process.env.DATABASEURL);
+
+var url = process.env.DATABASEURL || "mongodb://localhost/yelp_camp_v3";//set default value
+mongoose.connect(url);
 //mongoose.connect("mongodb://Blazblue:wang138953@ds125578.mlab.com:25578/yelpcamp_haichaowang");
+//mongodb://localhost/yelp_camp_v3
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
